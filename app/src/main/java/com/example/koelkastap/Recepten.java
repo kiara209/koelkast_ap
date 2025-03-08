61ac4d3c967fdb0fb1df99b2f404972956dc28d1
valerie-klmn <awikolman@gmail.com>
2025-03-08 22:15:34 +0100

    Files goeie namen, op elke pagina wat tekst (die titel pagina aanduid)

--- app/src/main/java/com/example/koelkastap/Recepten.java
+++ app/src/main/java/com/example/koelkastap/Recepten.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class account_aanmaken extends AppCompatActivity {
-
+public class Recepten extends AppCompatActivity {
+// SPELFOUT IN TITEL VAN DIT DOCUMENT, MAAR IDK OF JE DIE NAAM KAN VERANDEREN
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_account_aanmaken);
+        setContentView(R.layout.activity_recepten);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
