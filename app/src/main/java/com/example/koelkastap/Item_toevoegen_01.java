ba55db95774d8882a9f84b93a6713951f27dd71b
valerie-klmn <awikolman@gmail.com>
2025-03-19 13:14:01 +0100

    koelkastpage,navbar

--- app/src/main/java/com/example/koelkastap/Item_toevoegen_01.java
+++ app/src/main/java/com/example/koelkastap/Item_toevoegen_01.java
@@ -7,23 +7,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
-public class Recepten extends AppCompatActivity {
+public class Item_toevoegen_01 extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_recepten);
+        setContentView(R.layout.activity_item_toevoegen01);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        findViewById<BottomNavigationView>(R.id.bottom_nav_bar).setOnItemSelectedListener { item ->
-
-            return@setOnItemSelectedListener true
-        }
     }
 }
--- app/src/main/java/com/example/koelkastap/hoofd_koelkast.java
+++ app/src/main/java/com/example/koelkastap/hoofd_koelkast.java
@@ -7,8 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 public class hoofd_koelkast extends AppCompatActivity {
+
+    public Button button }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,7 @@ public class hoofd_koelkast extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        button = (Button) finfViewById(R.)
     }
 }
